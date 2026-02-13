994a1e291e0c468004effdd57727780d41042d0f
Carlos1026-web <Derek.carlos04@gmail.com>
2026-02-12 19:14:56 -0700

    Created homepage components and attached it to homepage. (CSS and styling not done yet)

---
+++ lifeline-canada-app/src/components/Homepage/HomepageNewsCompanionPaws.tsx
@@ -0,0 +1,20 @@
+export default function HomepageNewsCompanionPaws() {
+    return(
+        <div>
+            <h1>Companion Paws Canada</h1>
+
+            <h2>Companion Paws is dedicated to supporting Canadians in need while also providing a second chance for pets in rescues.</h2>
+
+            <h2>The main programs are:</h2>
+            <ul>
+                <li>Personal Therapy Dog Adoption Program</li>
+                <li>Assisted Therapy Dog Adoption Program</li>
+                <li>Certify Your Own Therapy Dog Program</li>
+            </ul>
+
+            <h2>These programs are available in all Provinces across Canada.  All information and the steps involved are at CompanionPaws.ca</h2>
+
+            <a href="">Learn More </a>
+        </div>
+    );
+}
